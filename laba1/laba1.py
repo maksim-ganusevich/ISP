e794d9ea65d47e41eb08a027551ffde8db4ca80f
Maksim Ganusevich <2001maksim49@gmail.com>
2021-03-26 20:40:12 +0300

    Updated laba1.py

--- laba1/laba1.py
+++ laba1/laba1.py
@@ -4,12 +4,14 @@ import math
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger = logging.getLogger()
 
+
 def solve(n):
-    return round(((1 + math.sqrt(5)) ** n)/2-((1-math.sqrt(5)) ** n)/2)/math.sqrt(5)
+    return int(((((1+math.sqrt(5))/2)**n)-(((1-math.sqrt(5))/2)**n))/math.sqrt(5))
 
 
 def main():
-	logger.info(solve(100))
+    logger.info(solve(1000))
+
 
 if __name__ == "__main__":
     main()
