f3f05228627b0b350f59093fa356e0df9022dd40
Maksim Ganusevich <2001maksim49@gmail.com>
2021-09-16 14:20:39 +0300

    added load/loads

--- laba2/yaml_serializer/yaml_serializer.py
+++ laba2/yaml_serializer/yaml_serializer.py
@@ -63,3 +63,119 @@ class Yaml:
             if res[-1] != '\n':
                 res += '\n'    
         return res
+
+    def loads(self, s):
+        self.pos = 0
+        return deconvert(self.from_str(s))
+
+    def load(self, fp):
+        with open(fp, 'r') as f:
+            return self.loads(f.read())
+    
+    def from_str(self, s, tab=''):
+        if self.pos >= len(s):
+            return
+        elif s[self.pos] in self.nums:
+            return self.from_str_num(s)
+        elif s[self.pos:self.pos+4] == 'null':
+            return self.from_str_null(s)
+        elif s[self.pos:self.pos+4] == 'true':
+            return self.from_str_true(s)
+        elif s[self.pos:self.pos+5] == 'false':
+            return self.from_str_false(s)
+        elif s[self.pos] == '[' or s[self.pos+len(tab)] == '-':
+            return self.from_str_collection(s, tab)
+        elif s[self.pos+len(tab):self.pos+len(tab)+2] == '  ' or s[self.pos] =='{':
+            return self.from_str_dict(s, '  ')
+        elif self.pos == 0:
+            return self.from_str_dict(s, tab)
+        else:
+            return self.from_str_str(s)
+
+    def from_str_str(self, s):
+        res = ""
+        opened = False
+        while self.pos < len(s) and s[self.pos] not in(':', '\n') or opened:
+            if s[self.pos] == "'":
+                if opened:
+                    opened = False
+                else:
+                    opened = True
+                self.pos += 1
+                continue
+            res += s[self.pos]
+            self.pos += 1        
+        self.pos += 1
+        return res
+
+    def from_str_num(self, s):
+        s_pos = self.pos
+        while self.pos < len(s) and (s[self.pos] in self.nums or s[self.pos] == '.'):
+            self.pos += 1
+        num = s[s_pos:self.pos]
+        self.pos += 1
+        return float(num) if '.' in str(num) else int(num)
+    
+    def from_str_null(self, s):
+        self.pos += 5
+        return None
+    
+    def from_str_true(self, s):
+        self.pos += 5
+        return True
+    
+    def from_str_false(self, s):
+        self.pos += 6
+        return False
+
+    def from_str_collection(self, s, tab):
+        res = []
+        self.pos += len(tab)
+        self.pos += 2
+        s_type = self.from_str_str(s)
+        if tab + '-' != s[self.pos:self.pos + len(tab) + 1]:
+            if s_type == '__tuple__':
+                return tuple(res)
+            elif s_type == '__set__':
+                return set(res)
+            return res
+        self.pos += len(tab)
+        while self.pos < len(s):
+            self.pos += 2
+            v = self.from_str(s)
+            res.append(v)
+            if tab + '-' != s[self.pos:self.pos + len(tab) + 1]:
+                break
+            self.pos += len(tab)
+        if s_type == '__tuple__':
+            return tuple(res)
+        elif s_type == '__set__':
+            return set(res)
+        return res
+
+    def from_str_dict(self, s, tab):
+        res = {}
+        if s[self.pos] == '{':
+            self.pos += 3
+            return res
+        while self.pos < len(s):
+            new_tab = ''
+            while s[self.pos] == ' ':
+                new_tab += ' '
+                self.pos += 1
+            if len(new_tab) >= len(tab):
+                tab = new_tab
+                k = self.from_str_str(s)
+                if self.pos >= len(s):
+                    return k
+                if s[self.pos] == ' ':
+                    self.pos += 1
+                    v = self.from_str(s, new_tab)
+                else:
+                    self.pos += 1
+                    v = self.from_str(s, new_tab)
+            elif len(new_tab) < len(tab):
+                self.pos -= len(new_tab)
+                return res
+            res[k] = v
+        return res
