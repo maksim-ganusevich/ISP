2b2b21370b05e2c8279573e367c401aa13efce31
Maksim Ganusevich <2001maksim49@gmail.com>
2021-09-16 13:18:19 +0300

    added load/loads

--- laba2/json_serializer/json_serializer.py
+++ laba2/json_serializer/json_serializer.py
@@ -64,4 +64,93 @@ class Json:
         self.pos += 2
         return res
     
+    def loads(self, s):
+        self.pos = 0
+        return deconvert(self.from_str(s))
+
+    def load(self, fp):
+        with open(fp, 'r') as f:
+            return self.loads(f.read())
+
+    def from_str(self, s):
+        if self.pos >= len(s):
+            return
+        if s[self.pos] in self.nums:
+            return self.from_str_num(s)
+        elif s[self.pos:self.pos+4] == 'null':
+            return self.from_str_null(s)
+        elif s[self.pos:self.pos+4] == 'true':
+            return self.from_str_true(s)
+        elif s[self.pos:self.pos+5] == 'false':
+            return self.from_str_false(s)
+        if s[self.pos] == '"':
+            return self.from_str_str(s)
+        if s[self.pos] == '[':
+            return self.from_str_collection(s)
+        if s[self.pos] == '{':
+            return self.from_str_dict(s)
+
+    def from_str_str(self, s):
+        res = ""
+        self.pos += 1
+        if s[self.pos] == '"':
+            self.pos += 1
+        while self.pos < len(s) and s[self.pos] not in ('"', "'"):
+            res += s[self.pos]
+            self.pos += 1        
+        self.pos += 1
+        return res
+
+    def from_str_num(self, s):
+        s_pos = self.pos
+        while self.pos < len(s) and (s[self.pos] in self.nums or s[self.pos] == '.'):
+            self.pos += 1
+        num = s[s_pos:self.pos]
+
+        return float(num) if '.' in str(num) else int(num)
     
+    def from_str_null(self, s):
+        self.pos += 4
+        return None
+    
+    def from_str_true(self, s):
+        self.pos += 4
+        return True
+    
+    def from_str_false(self, s):
+        self.pos += 5
+        return False
+
+    def from_str_collection(self, s):
+        res = []
+        self.pos += 1
+        s_type = self.from_str_str(s)
+        while self.pos < len(s) and s[self.pos] not in (']', '}', ')'):
+            if s[self.pos] == ' ' or s[self.pos] == ',':
+                self.pos += 1
+                continue
+            v = self.from_str(s)
+            res.append(v)
+            if self.pos < len(s) and s[self.pos] in (']', '}', ')'):
+                break
+            self.pos += 1
+        self.pos += 1
+        if s_type == '__tuple__':
+            return tuple(res)
+        elif s_type == '__set__':
+            return set(res)
+        return res
+
+    def from_str_dict(self, s):
+        res = {}
+        self.pos += 1
+        while self.pos < len(s) and s[self.pos] != '}':
+            while s[self.pos] in (' ', ','):
+                self.pos += 1
+                continue            
+            k = self.from_str_str(s)
+            self.pos = s.find(':', self.pos)+2
+            v = self.from_str(s)
+            res[k] = v
+        self.pos += 1
+        return res
